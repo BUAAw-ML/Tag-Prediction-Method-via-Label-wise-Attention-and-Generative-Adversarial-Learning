09d972d06b64e8e75b558cb1e73594cea23c1589
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-04 09:20:17 +0800

    update

--- main.py
+++ main.py
@@ -87,8 +87,8 @@ def multiLabel_text_classify():
     optimizer['Encoder'] = torch.optim.SGD(model['Encoder'].get_config_optim(args.lr, args.lrp), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
 
     optimizer['enc'] = torch.optim.SGD([{'params': model['Discriminator'].parameters(), 'lr': 0.1},
-                                        {'params': model['Encoder'].parameters(), 'lr': 0.01}], lr=0.001)
-    print(optimizer)
+                                        {'params': model['Encoder'].parameters(), 'lr': 0.01}], lr=0.001, momentum=args.momentum, weight_decay=args.weight_decay)
+    print(optimizer['enc'])
 
     state = {'batch_size': args.batch_size, 'max_epochs': args.epochs, 'evaluate': args.evaluate, 'resume': args.resume,
              'num_classes': dataset.get_tags_num(), 'difficult_examples': False,
