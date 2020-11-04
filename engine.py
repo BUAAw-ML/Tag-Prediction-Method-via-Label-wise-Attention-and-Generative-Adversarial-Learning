df10c1bbc0f3bf61e1fa7573673745943b13094f
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-04 14:43:28 +0800

    update

--- engine.py
+++ engine.py
@@ -438,11 +438,13 @@ class GCNMultiLabelMAPEngine(MultiLabelMAPEngine):
 
         D_fake_features, DU_fake_logits, DU_fake_prob = model['Discriminator'](x_g)
 
+        DU_fake_prob2 = DU_fake_prob.detach()
+
         D_L_unsupervised1U = -1 * torch.mean(torch.log(1 - D_real_prob[:, 0] + 1e-8))
         D_L_unsupervised2U = -1 * torch.mean(torch.log(DU_fake_prob[:, 0] + 1e-8))
         d_loss = D_L_Supervised + D_L_unsupervised1U + D_L_unsupervised2U
 
-        g_loss = -1 * torch.mean(torch.log(1 - DU_fake_prob[:, 0] + 1e-8))
+        g_loss = -1 * torch.mean(torch.log(1 - DU_fake_prob2[:, 0] + 1e-8))
         feature_error = torch.mean(D_real_features, dim=0) - torch.mean(D_fake_features, dim=0)
         G_feat_match = torch.mean(feature_error * feature_error)
         g_loss = g_loss #+ G_feat_match
@@ -460,7 +462,7 @@ class GCNMultiLabelMAPEngine(MultiLabelMAPEngine):
 
         if training:
             optimizer['enc'].zero_grad()
-            d_loss.backward(retain_graph=True) #
+            d_loss.backward() #
             nn.utils.clip_grad_norm_(optimizer['enc'].param_groups[0]["params"], max_norm=10.0)
             optimizer['enc'].step()
 
