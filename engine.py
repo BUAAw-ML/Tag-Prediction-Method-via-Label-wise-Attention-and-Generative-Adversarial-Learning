ffe48424dc5a0baa4c0528e5419a2e401b4b3550
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-04 10:31:35 +0800

    update

--- engine.py
+++ engine.py
@@ -443,9 +443,9 @@ class GCNMultiLabelMAPEngine(MultiLabelMAPEngine):
         d_loss = D_L_Supervised + D_L_unsupervised1U + D_L_unsupervised2U
 
         g_loss = -1 * torch.mean(torch.log(1 - DU_fake_prob[:, 0] + 1e-8))
-        # G_feat_match = torch.mean(
-        #     torch.sqrt(torch.mean(D_real_features, dim=0) - torch.mean(D_fake_features, dim=0)))
-        g_loss = g_loss #+ G_feat_match
+        feature_error = torch.mean(D_real_features, dim=0) - torch.mean(D_fake_features, dim=0)
+        G_feat_match = torch.mean(feature_error * feature_error)
+        g_loss = g_loss + G_feat_match
 
         self.state['loss'] = g_loss#d_loss #+
 
