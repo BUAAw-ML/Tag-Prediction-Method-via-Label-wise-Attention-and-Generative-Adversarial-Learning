2e706c25791a17ec75cbb12ba52895336c2a1710
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-04 10:21:09 +0800

    update

--- engine.py
+++ engine.py
@@ -432,23 +432,22 @@ class GCNMultiLabelMAPEngine(MultiLabelMAPEngine):
 
         D_L_Supervised = torch.mean(per_example_loss)
 
+        z = torch.rand(self.state['batch_size'], 3000).type(torch.FloatTensor).cuda(self.state['device_ids'][0])
 
-        # z = torch.rand(self.state['batch_size'], 3000).type(torch.FloatTensor).cuda(self.state['device_ids'][0])
-        #
-        # x_g = model['Generator'](z)
-        #
-        # D_fake_features, DU_fake_logits, DU_fake_prob = model['Discriminator'](x_g)
-        #
-        # D_L_unsupervised1U = -1 * torch.mean(torch.log(1 - D_real_prob[:, 0] + 1e-8))
-        # D_L_unsupervised2U = -1 * torch.mean(torch.log(DU_fake_prob[:, 0] + 1e-8))
-        d_loss = D_L_Supervised #+ D_L_unsupervised1U + D_L_unsupervised2U
+        x_g = model['Generator'](z)
 
-        # g_loss = -1 * torch.mean(torch.log(1 - DU_fake_prob[:, 0] + 1e-8))
-        # G_feat_match = torch.mean(
-        #     torch.sqrt(torch.mean(D_real_features, dim=0) - torch.mean(D_fake_features, dim=0)))
-        # g_loss = g_loss + G_feat_match
+        D_fake_features, DU_fake_logits, DU_fake_prob = model['Discriminator'](x_g)
 
-        self.state['loss'] = d_loss #+ g_loss
+        D_L_unsupervised1U = -1 * torch.mean(torch.log(1 - D_real_prob[:, 0] + 1e-8))
+        D_L_unsupervised2U = -1 * torch.mean(torch.log(DU_fake_prob[:, 0] + 1e-8))
+        d_loss = D_L_Supervised + D_L_unsupervised1U + D_L_unsupervised2U
+
+        g_loss = -1 * torch.mean(torch.log(1 - DU_fake_prob[:, 0] + 1e-8))
+        G_feat_match = torch.mean(
+            torch.sqrt(torch.mean(D_real_features, dim=0) - torch.mean(D_fake_features, dim=0)))
+        g_loss = g_loss + G_feat_match
+
+        self.state['loss'] = g_loss#d_loss #+
 
         if training:
             self.state['train_iters'] += 1
@@ -461,7 +460,7 @@ class GCNMultiLabelMAPEngine(MultiLabelMAPEngine):
 
         if training:
             optimizer['enc'].zero_grad()
-            d_loss.backward() #retain_graph=True
+            d_loss.backward(retain_graph=True) #
             nn.utils.clip_grad_norm_(optimizer['enc'].param_groups[0]["params"], max_norm=10.0)
             optimizer['enc'].step()
 
@@ -475,10 +474,10 @@ class GCNMultiLabelMAPEngine(MultiLabelMAPEngine):
             # nn.utils.clip_grad_norm_(model['Encoder'].parameters(), max_norm=10.0)
             # optimizer['Encoder'].step()
 
-            # optimizer['Generator'].zero_grad()
-            # g_loss.backward()
-            # nn.utils.clip_grad_norm_(model['Generator'].parameters(), max_norm=10.0)
-            # optimizer['Generator'].step()
+            optimizer['Generator'].zero_grad()
+            g_loss.backward()
+            nn.utils.clip_grad_norm_(model['Generator'].parameters(), max_norm=10.0)
+            optimizer['Generator'].step()
 
         else:
             return self.state['output']
