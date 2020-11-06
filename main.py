e4f00fa5943eae2413fa1264687d8a3454983271
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-06 12:49:21 +0800

    update

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ parser.add_argument('--data_type', default='allData', type=str,
                     help='The type of data')
 parser.add_argument('--data_path', default='../datasets/ProgrammerWeb/programweb-data.csv', type=str,
                     help='path of data')
-parser.add_argument('--utilize_unlabeled_data', default=False, type=bool,
+parser.add_argument('--utilize_unlabeled_data', default=True, type=bool,
                     help='utilize_unlabeled_data')
 
 #../../datasets/multiClass_text_classification/news_group20/news_group20.csv
