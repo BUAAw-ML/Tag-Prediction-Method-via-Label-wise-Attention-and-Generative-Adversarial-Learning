892cd8914f27e9888e3659ef476c8a9b77c39530
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-07 13:22:20 +0800

    update

--- engine.py
+++ engine.py
@@ -530,9 +530,9 @@ class GCNMultiLabelMAPEngine(MultiLabelMAPEngine):
                 log_probs = F.log_softmax(logits, dim=-1)
                 per_example_loss = -1 * torch.sum(target_var * log_probs, dim=-1) / target_var.shape[-1]
                 D_L_Supervised = torch.mean(per_example_loss)
-                d_loss = D_L_Supervised + D_L_unsupervised1U + D_L_unsupervised2U
+                d_loss = D_L_Supervised + D_L_unsupervised1U #+ D_L_unsupervised2U
             else:
-                d_loss = D_L_unsupervised1U + D_L_unsupervised2U
+                d_loss = D_L_unsupervised1U #+ D_L_unsupervised2U
 
             d_loss.backward()  #
             nn.utils.clip_grad_norm_(optimizer['enc'].param_groups[0]["params"], max_norm=10.0)
@@ -554,7 +554,7 @@ class GCNMultiLabelMAPEngine(MultiLabelMAPEngine):
             g_loss = -1 * torch.mean(torch.log(1 - DU_fake_prob[:, 0] + 1e-8))
             feature_error = torch.mean(D_real_features2, dim=0) - torch.mean(D_fake_features, dim=0)
             G_feat_match = torch.mean(feature_error * feature_error)
-            g_loss = g_loss #+ G_feat_match
+            g_loss = g_loss + G_feat_match
 
             g_loss.backward()
             nn.utils.clip_grad_norm_(model['Generator'].parameters(), max_norm=10.0)
