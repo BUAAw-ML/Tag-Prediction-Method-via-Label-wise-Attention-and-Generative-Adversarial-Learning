fa750db6d03bd864410b2b7faeeca1d2c357979b
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-08 10:11:46 +0800

    update

--- models.py
+++ models.py
@@ -122,7 +122,7 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
 
         self.dropout = nn.Dropout(p=0.5)
-        self.act = nn.Tanh()#nn.LeakyReLU(0.2)
+        self.act = nn.Sigmoid()#nn.LeakyReLU(0.2)
 
         self.num_hidden_generator = num_hidden_generator
         self.hidden_list_generator = nn.ModuleList()
