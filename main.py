4b30cedcc9f98d04ba8c2a963485f1a4c77457dc
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-11 16:05:33 +0800

    update

--- main.py
+++ main.py
@@ -40,11 +40,11 @@ parser.add_argument('--save_model_path', default='./checkpoint', type=str,
                     help='path to save checkpoint (default: none)')
 parser.add_argument('--log_dir', default='./logs', type=str,
                     help='path to save log (default: none)')
-parser.add_argument('--data_type', default='allData', type=str,
+parser.add_argument('--data_type', default='TrainTestData', type=str,
                     help='The type of data')
-parser.add_argument('--data_path', default='../datasets/ProgrammerWeb/programweb-data.csv', type=str,
+parser.add_argument('--data_path', default='../datasets/ProgrammerWeb', type=str,
                     help='path of data')
-parser.add_argument('--utilize_unlabeled_data', default=True, type=bool,
+parser.add_argument('--utilize_unlabeled_data', default=False, type=bool,
                     help='utilize_unlabeled_data')
 #/programweb-data.csv
 #../../datasets/multiClass_text_classification/news_group20/news_group20.csv
