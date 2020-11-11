24a7c539f62fa7da220f1955562818cfc3e447ea
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-11 11:51:13 +0800

    update

--- engine.py
+++ engine.py
@@ -527,9 +527,9 @@ class GCNMultiLabelMAPEngine(MultiLabelMAPEngine):
                 log_probs = F.log_softmax(logits, dim=-1)
                 per_example_loss = -1 * torch.sum(target_var * log_probs, dim=-1) / target_var.shape[-1]
                 D_L_Supervised = torch.mean(per_example_loss)
-                d_loss = D_L_Supervised + D_L_unsupervised1U #+ D_L_unsupervised2U
+                d_loss = D_L_Supervised + D_L_unsupervised1U + D_L_unsupervised2U
             else:
-                d_loss = D_L_unsupervised1U# + D_L_unsupervised2U
+                d_loss = D_L_unsupervised1U + D_L_unsupervised2U
 
             d_loss.backward()  #
             nn.utils.clip_grad_norm_(optimizer['enc'].param_groups[0]["params"], max_norm=10.0)
