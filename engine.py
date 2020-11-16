ba1b111ef20ee2ae5d347b5bba4ad41d0026f8be
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-16 11:08:24 +0800

    update

--- engine.py
+++ engine.py
@@ -517,22 +517,31 @@ class GCNMultiLabelMAPEngine(MultiLabelMAPEngine):
             logits = D_real_logits[:, 1:]
             self.state['output'] = F.softmax(logits, dim=-1)
 
-            D_fake_features, DU_fake_logits, DU_fake_prob = model['Discriminator'](x_g2)
+            # D_fake_features, DU_fake_logits, DU_fake_prob = model['Discriminator'](x_g2)
 
             D_L_unsupervised1U = -1 * torch.mean(torch.log(1 - D_real_prob[:, 0] + 1e-8))
-            D_L_unsupervised2U = -1 * torch.mean(torch.log(DU_fake_prob[:, 0] + 1e-8))
+            # D_L_unsupervised2U = -1 * torch.mean(torch.log(DU_fake_prob[:, 0] + 1e-8))
 
             if semi_supervised == False:
                 log_probs = F.log_softmax(logits, dim=-1)
                 per_example_loss = -1 * torch.sum(target_var * log_probs, dim=-1) / target_var.shape[-1]
                 D_L_Supervised = torch.mean(per_example_loss)
-                d_loss = D_L_Supervised + D_L_unsupervised1U + D_L_unsupervised2U
+                d_loss = D_L_Supervised + D_L_unsupervised1U #+ D_L_unsupervised2U
             else:
-                d_loss = D_L_unsupervised1U + D_L_unsupervised2U
+                d_loss = D_L_unsupervised1U #+ D_L_unsupervised2U
 
             optimizer['enc'].zero_grad()
             d_loss.backward()  #
+            nn.utils.clip_grad_norm_(optimizer['enc'].param_groups[0]["params"], max_norm=10.0)
+            optimizer['enc'].step()
 
+            D_fake_features, DU_fake_logits, DU_fake_prob = model['Discriminator'](x_g2)
+            D_L_unsupervised2U = -1 * torch.mean(torch.log(DU_fake_prob[:, 0] + 1e-8))
+
+            d_loss = D_L_unsupervised2U
+
+            optimizer['enc'].zero_grad()
+            d_loss.backward()  #
             nn.utils.clip_grad_norm_(optimizer['enc'].param_groups[0]["params"], max_norm=10.0)
             optimizer['enc'].step()
 
