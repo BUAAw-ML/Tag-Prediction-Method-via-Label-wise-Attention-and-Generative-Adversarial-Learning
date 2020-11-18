a2811c068aa5a258f0c1a02a8ae6a5fba28e007d
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-18 16:52:26 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -59,10 +59,10 @@ class allData(Dataset):
 
         data = np.array(data)
         ind = np.random.RandomState(seed=10).permutation(len(data))
-        split = int(len(data) * 0.7)
+        split = int(len(data) * 0.05)
         split2 = int(len(data) * 0.7)
         train_data = data[ind[:split]].tolist()
-        unlabeled_train_data = []#data[ind[split:split2]].tolist()
+        unlabeled_train_data = data[ind[split:split2]].tolist()
         test_data = data[ind[split2:]].tolist()
 
         co_occur_mat = allData.stat_cooccurence(data, len(tag2id))
