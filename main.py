86bde7bed57562812b0138c989726506185b73d3
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-18 16:12:46 +0800

    update

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ parser.add_argument('--data_type', default='allData', type=str,
                     help='The type of data')
 parser.add_argument('--data_path', default='../datasets/ProgrammerWeb/programweb-data.csv', type=str,
                     help='path of data')
-parser.add_argument('--utilize_unlabeled_data', default=False, type=bool,
+parser.add_argument('--utilize_unlabeled_data', default=True, type=bool,
                     help='utilize_unlabeled_data')
 
 #../datasets/ProgrammerWeb/programweb-data.csv
