96811788c8759296e12b39f77a2adc22345cc874
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-19 09:24:08 +0800

    update

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
 parser.add_argument('-b', '--batch-size', default=4, type=int,
                     metavar='N', help='mini-batch size (default: 256)')
-parser.add_argument('--G-lr', '--Generator-learning-rate', default=0.0001, type=float,
+parser.add_argument('--G-lr', '--Generator-learning-rate', default=0.001, type=float,
                     metavar='LR', help='initial learning rate')
 parser.add_argument('--D-lr', '--Discriminator-learning-rate', default=0.1, type=float,
                     metavar='LR', help='learning rate for pre-trained layers')
