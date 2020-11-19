1af197848d5920c4da03a09d48104742bb950959
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-19 17:28:11 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -59,8 +59,8 @@ class allData(Dataset):
 
         data = np.array(data)
         ind = np.random.RandomState(seed=10).permutation(len(data))
-        split = int(len(data) * 0.2)
-        split2 = int(len(data) * 0.7)
+        split = int(len(data) * 0.05)
+        split2 = int(len(data) * 0.9)
         train_data = data[ind[:split]].tolist()
         unlabeled_train_data = data[ind[split:split2]].tolist()
         test_data = data[ind[split2:]].tolist()
@@ -152,7 +152,7 @@ class allData(Dataset):
         # ignored_tags = set(['Tools','Applications','Other', 'API', 'Software-as-a-Service','Platform-as-a-Service',
         # 'Data-as-a-Service'])  #
         for tag in tag_occurance:
-            if tag_occurance[tag] > 200:
+            if tag_occurance[tag] == 0:
                 ignored_tags.add(tag)
 
         print(ignored_tags)
