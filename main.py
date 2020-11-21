8fc800a2e66d4ed45d9788c27a3a798704a5c7de
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-21 12:10:35 +0800

    update

--- main.py
+++ main.py
@@ -45,9 +45,9 @@ parser.add_argument('--save_model_path', default='./checkpoint', type=str,
                     help='path to save checkpoint (default: none)')
 parser.add_argument('--log_dir', default='./logs', type=str,
                     help='path to save log (default: none)')
-parser.add_argument('--data_type', default='allData', type=str,
+parser.add_argument('--data_type', default='TrainTestData', type=str,
                     help='The type of data')
-parser.add_argument('--data_path', default='../datasets/ProgrammerWeb/programweb-data.csv', type=str,
+parser.add_argument('--data_path', default='../datasets/ProgrammerWeb', type=str,
                     help='path of data')
 parser.add_argument('--utilize_unlabeled_data', default=True, type=bool,
                     help='utilize_unlabeled_data')
