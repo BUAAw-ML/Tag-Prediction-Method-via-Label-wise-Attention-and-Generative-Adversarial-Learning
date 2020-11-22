2b455f36f70ad8780e579ab2309ad60a0407dfd3
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-22 10:35:27 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -431,7 +431,7 @@ def load_TrainTestData(data_path):
 
     if os.path.isfile(os.path.join('cache', cache_file_head + '.dataset')) \
             and os.path.isfile(os.path.join('cache', cache_file_head + '.encoded_tag')) \
-            and os.path.isfile(os.path.join('cache', cache_file_head + '.tag_mask')) and False:
+            and os.path.isfile(os.path.join('cache', cache_file_head + '.tag_mask')):
 
         print("load dataset from cache")
 
