c604e218ce098cfde2462436f6103d2296e9ed15
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-22 18:51:04 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -282,7 +282,7 @@ def load_allData(data_path=None):
 
     if os.path.isfile(os.path.join('cache', cache_file_head + '.dataset')) \
             and os.path.isfile(os.path.join('cache', cache_file_head + '.encoded_tag')) \
-            and os.path.isfile(os.path.join('cache', cache_file_head + '.tag_mask')):
+            and os.path.isfile(os.path.join('cache', cache_file_head + '.tag_mask')) and False:
 
         print("load dataset from cache")
 
