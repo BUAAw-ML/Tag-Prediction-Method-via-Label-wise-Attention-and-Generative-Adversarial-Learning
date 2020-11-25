98950de55a9d2d20b4dd60be1e0997aa2ea6ce67
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-25 08:06:57 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -3,17 +3,17 @@
 
 python main.py \
         --epochs=50 \
-        --epoch_step=40 \
+        --epoch_step=13 \
         --device_ids=0 \
         --batch-size=10 \
         --G-lr=0.001 \
         --D-lr=0.1 \
-        --B-lr=0.001 \
+        --B-lr=0.01 \
         --print-freq=200 \
         --save_model_path='./checkpoint' \
         --log_dir='./logs' \
         --data_type='allData' \
-        --data_path='../datasets/ProgrammerWeb/programweb-data.csv' \
+        --data_path='../datasets/AAPD/aapd2.csv' \
         --utilize_unlabeled_data=1 \
 
 
