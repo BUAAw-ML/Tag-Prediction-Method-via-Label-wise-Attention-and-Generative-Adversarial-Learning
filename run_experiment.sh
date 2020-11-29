73b2bfaa7e6fd71502f5843608a808aafcc23189
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-29 08:51:30 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -16,24 +16,8 @@ python main.py \
         --data_path='../datasets/EUR-Lex' \
         --utilize_unlabeled_data=0 \
         --use_previousData=1 \
-        --method='MultiLabelMAP' \
+        --method='semiGAN_MultiLabelMAP' \
 
-python main.py \
-        --epochs=50 \
-        --epoch_step=13 \
-        --device_ids=0 \
-        --batch-size=10 \
-        --G-lr=0.001 \
-        --D-lr=0.1 \
-        --B-lr=0.01 \
-        --print-freq=200 \
-        --save_model_path='./checkpoint' \
-        --log_dir='./logs' \
-        --data_type='TrainTestTextTag' \
-        --data_path='../datasets/EUR-Lex' \
-        --utilize_unlabeled_data=1 \
-        --use_previousData=1 \
-        --method='MultiLabelMAP' \
 
 #All  TrainTest  TrainTestTextTag
 #MultiLabelMAP semiGAN_MultiLabelMAP
