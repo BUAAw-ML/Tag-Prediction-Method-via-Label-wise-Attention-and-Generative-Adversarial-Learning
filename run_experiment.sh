0af62a21ab43daeb8998fde3ccbbd898b72a45ec
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-30 19:41:52 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -27,7 +27,7 @@ python main.py \
         --batch-size=8 \
         --G-lr=0.001 \
         --D-lr=0.1 \
-        --B-lr=0.01 \
+        --B-lr=0.001 \
         --print-freq=200 \
         --save_model_path='./checkpoint' \
         --data_type='TrainTestTextTag' \
