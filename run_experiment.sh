d301a473f794eb92afa37538af4243f71f06761c
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-02 08:12:44 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -4,29 +4,9 @@
 #EN='02'  #experiment_no
 
 python main.py \
-        --experiment_no='tagFrequence>100_dataSplit=0.05'  \
-        --epochs=70 \
-        --epoch_step=65 \
-        --device_ids=0 \
-        --batch-size=8 \
-        --G-lr=0.001 \
-        --D-lr=0.1 \
-        --B-lr=0.01 \
-        --save_model_path='./checkpoint' \
-        --data_type='TrainTestTextTag' \
-        --data_path='../datasets/EUR-Lex' \
-        --use_previousData=0 \
-        --method='semiGAN_MultiLabelMAP' \
-        --overlength_handle='skip' \
-        --min_tagFrequence=100  \
-        --max_tagFrequence=20000  \
-        --intanceNum_limit=10000 \
-        --data_split=0.05  \
-
-python main.py \
-        --experiment_no='tagFrequence>100_dataSplit=0.05'  \
-        --epochs=45 \
-        --epoch_step=35 \
+        --experiment_no='tagFrequence>100_datasplit=0.05'  \
+        --epochs=50 \
+        --epoch_step=40 \
         --device_ids=0 \
         --batch-size=8 \
         --G-lr=0.001 \
@@ -42,6 +22,28 @@ python main.py \
         --max_tagFrequence=20000  \
         --intanceNum_limit=10000 \
         --data_split=0.05  \
+
+python main.py \
+        --experiment_no='tagFrequence>100_datasplit=0.05'  \
+        --epochs=80 \
+        --epoch_step=70 \
+        --device_ids=0 \
+        --batch-size=8 \
+        --G-lr=0.001 \
+        --D-lr=0.1 \
+        --B-lr=0.001 \
+        --save_model_path='./checkpoint' \
+        --data_type='TrainTestTextTag' \
+        --data_path='../datasets/EUR-Lex' \
+        --use_previousData=0 \
+        --method='semiGAN_MultiLabelMAP' \
+        --overlength_handle='skip' \
+        --min_tagFrequence=100  \
+        --max_tagFrequence=20000  \
+        --intanceNum_limit=10000 \
+        --data_split=0.05  \
+
+
 
 
 #方法、学习率、epoch_step
