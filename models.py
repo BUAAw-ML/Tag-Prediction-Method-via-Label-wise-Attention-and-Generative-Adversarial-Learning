a990301d36be2abd14c4e0ff74a90abea55af46e
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-03 11:23:53 +0800

    update

--- models.py
+++ models.py
@@ -43,7 +43,7 @@ class MABert(nn.Module):
         attention_out = attention @ token_feat   # N, labels_num, hidden_size
         attention_out = attention_out * self.class_weight
         #################fake sample process#######
-        # feat = feat[:,:token_feat.shape[1],:] # N, L, hidden_size
+        feat = feat[:,:token_feat.shape[1],:] # N, L, hidden_size
         feat = torch.mean(feat, 1)
         # attention_fake = (torch.matmul(feat, tag_embedding.transpose(0, 1))).transpose(1, 2).masked_fill(
         #     (1 - masks.byte()), torch.tensor(-np.inf))
