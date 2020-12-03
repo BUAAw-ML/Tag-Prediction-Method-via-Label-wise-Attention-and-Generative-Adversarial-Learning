4f12f9fbc33a069cdb533a988826fbf86792aaee
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-03 13:40:46 +0800

    update

--- engine.py
+++ engine.py
@@ -486,8 +486,9 @@ class semiGAN_MultiLabelMAPEngine(MultiLabelMAPEngine):
 
         g_loss = -1 * torch.mean(torch.log(1 - prob[:, 0] + 1e-8))
         # feature_error = torch.mean(torch.mean(features.detach(), dim=0) - torch.mean(x_g[:,:features.shape[1],:], dim=0), dim=0)
-        # G_feat_match = torch.mean(feature_error * feature_error)
-        g_loss = g_loss #+ G_feat_match
+        feature_error = torch.mean(torch.mean(features.detach(), dim=0) - torch.mean(x_g, dim=0), dim=0)
+        G_feat_match = torch.mean(feature_error * feature_error)
+        g_loss = G_feat_match #g_loss #+
 
         if training:
             optimizer['Generator'].zero_grad()
--- run_experiment.sh
+++ run_experiment.sh
@@ -3,28 +3,9 @@
 
 #EN='02'  #experiment_no
 
-python main.py \
-        --experiment_no='max_tagFrequence<200_intanceNum_limit=10000'  \
-        --epochs=50 \
-        --epoch_step=30 \
-        --device_ids=0 \
-        --batch-size=8 \
-        --G-lr=0.001 \
-        --D-lr=0.1 \
-        --B-lr=0.01 \
-        --save_model_path='./checkpoint' \
-        --data_type='TrainTestTextTag' \
-        --data_path='../datasets/stack-overflow' \
-        --use_previousData=0 \
-        --method='MultiLabelMAP' \
-        --overlength_handle='skip' \
-        --min_tagFrequence=0  \
-        --max_tagFrequence=200  \
-        --intanceNum_limit=10000 \
-        --data_split=0.1  \
 
 python main.py \
-        --experiment_no='max_tagFrequence<200_intanceNum_limit=10000'  \
+        --experiment_no='max_tagFrequence<200_intanceNum_limit=10000_Test'  \
         --epochs=90 \
         --epoch_step=75 \
         --device_ids=0 \
