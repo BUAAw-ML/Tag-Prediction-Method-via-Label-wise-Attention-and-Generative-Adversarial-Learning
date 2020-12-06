27d5fb42cc5173450f741908be80595fe8b78725
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-06 11:40:31 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -7,24 +7,24 @@
 
 python main.py \
         --experiment_no='tagFrequence<200_Test'  \
-        --epochs=50 \
-        --epoch_step=40 \
+        --epochs=20 \
+        --epoch_step=15 \
         --device_ids=0 \
         --batch-size=4 \
         --G-lr=0.1 \
-        --D-lr=0.001 \
+        --D-lr=0.1 \
         --B-lr=0.001 \
         --save_model_path='./checkpoint' \
         --data_type='All' \
         --data_path='../datasets/ProgrammerWeb/programweb-data.csv' \
         --use_previousData=0 \
-        --method='semiGAN_MultiLabelMAP' \
+        --method='MultiLabelMAP' \
         --overlength_handle='skip' \
         --min_tagFrequence=0  \
         --max_tagFrequence=200  \
         --intanceNum_limit=200000 \
         --data_split=0.05  \
-        --test_description='+G_feat_match'  \
+        --test_description='Linear'  \
 
 
 
