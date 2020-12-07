43a7fdbdc2b5a2d6171d743837a1b8a31b84de85
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-07 12:56:10 +0800

    update

--- engine.py
+++ engine.py
@@ -446,7 +446,7 @@ class semiGAN_MultiLabelMAPEngine(MultiLabelMAPEngine):
 
         # z = torch.randn(ids.shape[0], 768).type(torch.FloatTensor).cuda(self.state['device_ids'][0])
 
-        z = torch.Tensor(ids.shape[0], 768).uniform_(0, 1).cuda(self.state['device_ids'][0])
+        z = torch.Tensor(ids.shape[0], 768).uniform_(-1, 1).cuda(self.state['device_ids'][0])
 
         x_g = model['Generator'](z)
 
@@ -463,7 +463,7 @@ class semiGAN_MultiLabelMAPEngine(MultiLabelMAPEngine):
         # epsion2 = torch.zeros((4, 1)).cuda(self.state['device_ids'][0])
         # epsion2[prob == 1] = 1e-8
         # D_L_unsupervised = -1 * torch.mean(torch.log(1 - prob + epsion2))
-        D_L_unsupervised = -1 * torch.mean(torch.log(prob + epsilon))
+        D_L_unsupervised = -1 * torch.mean(torch.log(1 - prob + epsilon))
 
         if semi_supervised == False: #train with labeled data
             log_probs = F.log_softmax(logits, dim=-1)
@@ -498,7 +498,7 @@ class semiGAN_MultiLabelMAPEngine(MultiLabelMAPEngine):
         # epsion = torch.zeros((4, 1)).cuda(self.state['device_ids'][0])
         # epsion[prob == 0] = 1e-8
         # g_loss = -1 * torch.mean(torch.log(prob + epsion))
-        g_loss = -1 * torch.mean(torch.log(1 - prob + epsilon))
+        g_loss = -1 * torch.mean(torch.log(prob + epsilon))
         # feature_error = torch.mean(torch.mean(features.detach(), dim=0) - torch.mean(x_g[:,:features.shape[1],:], dim=0), dim=0)
         # feature_error = torch.mean(torch.mean(features.detach(), dim=0) - torch.mean(x_g, dim=0), dim=0)
         # G_feat_match = torch.mean(feature_error * feature_error)
