5df0431c8f3bd53d66b25736d0535be7e9a0aa17
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-07 15:40:31 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -3,8 +3,6 @@
 
 #EN='02'  #experiment_no
 
-
-
 python main.py \
         --experiment_no='tagFrequence<1000_Test'  \
         --epochs=50 \
@@ -15,16 +13,37 @@ python main.py \
         --D-lr=0.1 \
         --B-lr=0.01 \
         --save_model_path='./checkpoint' \
-        --data_type='TrainTest_agNews' \
-        --data_path='../datasets/ag-news' \
-        --use_previousData=1 \
+        --data_type='All' \
+        --data_path='../datasets/ProgrammerWeb/programweb-data.csv' \
+        --use_previousData=0 \
         --method='MultiLabelMAP' \
         --overlength_handle='skip' \
         --min_tagFrequence=0  \
-        --max_tagFrequence=1000  \
+        --max_tagFrequence=200  \
         --intanceNum_limit=10000 \
-        --data_split=0.01  \
+        --data_split=0.05  \
         --test_description=''  \
+
+#python main.py \
+#        --experiment_no='tagFrequence<1000_Test'  \
+#        --epochs=50 \
+#        --epoch_step=40 \
+#        --device_ids=0 \
+#        --batch-size=4 \
+#        --G-lr=0.1 \
+#        --D-lr=0.1 \
+#        --B-lr=0.01 \
+#        --save_model_path='./checkpoint' \
+#        --data_type='TrainTest_agNews' \
+#        --data_path='../datasets/ag-news' \
+#        --use_previousData=1 \
+#        --method='MultiLabelMAP' \
+#        --overlength_handle='skip' \
+#        --min_tagFrequence=0  \
+#        --max_tagFrequence=1000  \
+#        --intanceNum_limit=10000 \
+#        --data_split=0.01  \
+#        --test_description=''  \
 
 
 #方法、epoch_step
