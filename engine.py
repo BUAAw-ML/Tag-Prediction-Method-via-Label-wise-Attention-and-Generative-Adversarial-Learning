f4910f719d2296359b49c67b7bc88f0b440feecb
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-08 11:16:15 +0800

    update

--- engine.py
+++ engine.py
@@ -341,7 +341,6 @@ class MultiLabelMAPEngine(Engine):
         # per_example_loss = -1 * torch.sum(target_var * log_probs, dim=-1) / target_var.shape[-1]
         # D_L_Supervised = torch.mean(per_example_loss)
 
-        self.state['output'] = torch.sigmoid(logits)
 
         self.state['loss'] = criterion(self.state['output'], target_var)
 
