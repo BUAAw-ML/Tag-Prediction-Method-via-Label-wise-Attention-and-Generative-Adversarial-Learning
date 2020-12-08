344177bfe73bed7edc73263d5feb331c5f222b7b
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-08 11:17:19 +0800

    update

--- models.py
+++ models.py
@@ -46,7 +46,7 @@ class MABert(nn.Module):
         attention = (torch.matmul(token_feat, tag_embedding.detach().transpose(0, 1))).transpose(1, 2).masked_fill((1 - masks.byte()), torch.tensor(-np.inf))
         attention = F.softmax(attention, -1)
         attention_out = attention @ token_feat   # N, labels_num, hidden_size
-        attention_out = attention_out * self.class_weight
+        # attention_out = attention_out * self.class_weight
         logit = torch.sum(attention_out, -1)
         logit = torch.sigmoid(logit)
         #################fake sample process#######
