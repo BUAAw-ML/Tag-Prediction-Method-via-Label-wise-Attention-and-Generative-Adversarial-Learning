4660c857fd5e211c14279b2c4a1b42fa764fb38c
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-09 10:13:20 +0800

    update

--- engine.py
+++ engine.py
@@ -480,13 +480,13 @@ class semiGAN_MultiLabelMAPEngine(MultiLabelMAPEngine):
             # log_probs = F.log_softmax(logits, dim=-1)
             # per_example_loss = -1 * torch.sum(pseudo_label * log_probs, dim=-1) / pseudo_label.shape[-1]
             # D_L_Supervised = torch.mean(per_example_loss)
-            # print(logits)
-            # print("features：")
-            # print(prob)
+            print(logits)
+            print("features：")
+            print(prob)
 
             D_L_unsupervised2 = -1 * torch.mean(torch.log(prob2 + epsilon))
 
-            d_loss = D_L_unsupervised + D_L_unsupervised2
+            d_loss = D_L_unsupervised
 
         if training:
             optimizer['enc'].zero_grad()
