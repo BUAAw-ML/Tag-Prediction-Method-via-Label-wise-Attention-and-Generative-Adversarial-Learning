2875775d6dc1817d5e544161d2008e6aac786ecf
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-09 09:26:49 +0800

    update

--- models.py
+++ models.py
@@ -52,12 +52,12 @@ class MABert(nn.Module):
         attention = F.softmax(attention, -1)
         attention_out = attention @ token_feat   # N, labels_num, hidden_size
         # attention_out = attention_out * self.class_weight
-        # logit = torch.sum(attention_out, -1)
+        logit = torch.sum(attention_out, -1)
         # logit = torch.sigmoid(logit)
         #################fake sample process#######
         feat = feat[:,:token_feat.shape[1],:] # N, L, hidden_size
         # feat = torch.mean(feat, 1)
-        attention_fake = (torch.matmul(feat, tag_embedding.detach().transpose(0, 1))).transpose(1, 2).masked_fill(
+        attention_fake = (torch.matmul(feat, tag_embedding.transpose(0, 1))).transpose(1, 2).masked_fill(
             (1 - masks.byte()), torch.tensor(-np.inf))
 
         # similarity_fake = (torch.matmul(feat, tag_embedding.transpose(0, 1))).transpose(1, 2).masked_fill(
@@ -67,19 +67,20 @@ class MABert(nn.Module):
         attention_fake = F.softmax(attention_fake, -1)
         attention_out_fake = attention_fake @ feat  # N, labels_num, hidden_size
         # discrimate = torch.matmul(feat, tag_embedding.transpose(0, 1))
-        # discrimate = torch.sum(attention_out_fake, -1)
+        discrimate = torch.sum(attention_out_fake, -1)
         # attention_out_fake = attention_out_fake * self.class_weight
         # discrimate = torch.mean(attention_out_fake, -2, keepdim=True)
         # discrimate = torch.sum(discrimate, -1, keepdim=True)
         #################
+        pred = torch.cat((logit, discrimate), -1)
 
         # discrimate = torch.sum(torch.matmul(feat, self.class_weight.transpose(0, 1)), -1, keepdim=True)
         # attention_out = attention_out * self.class_weight
 
-        pred = torch.cat((attention_out_fake, attention_out), -2)
-        pred = self.Linear1(pred)
-        pred = self.act(pred)
-        pred = self.Linear2(pred).squeeze(-1)
+        # pred = torch.cat((attention_out_fake, attention_out), -2)
+        # pred = self.Linear1(pred)
+        # pred = self.act(pred)
+        # pred = self.Linear2(pred).squeeze(-1)
         pred = torch.sigmoid(pred)
 
         logit = pred[:,self.num_classes:]
