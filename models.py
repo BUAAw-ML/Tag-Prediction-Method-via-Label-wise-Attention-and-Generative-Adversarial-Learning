a11d70b344d0badca5a11135acee524e1104f4a8
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-13 17:39:08 +0800

    update

--- models.py
+++ models.py
@@ -126,6 +126,7 @@ class MABert(nn.Module):
         masks = torch.unsqueeze(attention_mask, 1)  # N, 1, L  .bool()
         attention = (torch.matmul(token_feat, tag_embedding.transpose(0, 1))).transpose(1, 2).masked_fill((1 - masks.byte()), torch.tensor(-np.inf))
         attention = F.softmax(attention, -1)
+        print("---")
         print(torch.max(torch.max(attention,-1)[0],-1)[1])
         attention_out = attention @ token_feat   # N, labels_num, hidden_size
 
