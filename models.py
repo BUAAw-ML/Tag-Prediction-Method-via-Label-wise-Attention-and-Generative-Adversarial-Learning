02a7fe9f632b21487d951de01cd2fa07ec24ba3f
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-18 08:55:38 +0800

    update

--- models.py
+++ models.py
@@ -98,8 +98,10 @@ class MABert(nn.Module):
         attention_out_fake = torch.sum(attention_out_fake, -1, keepdim=True)
 
         prob = torch.cat((attention_out_fake,attention_out),-1)
-
+        print(prob)
+        print(self.output(prob))
         prob = self.output(prob)[:,0]
+
 
         #################
         # masks = torch.unsqueeze(attention_mask, 1)  # N, 1, L  .bool()
