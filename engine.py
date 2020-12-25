25703c1c39cc499b6b1e3f2a4649184528757527
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-25 13:50:22 +0800

    update

--- engine.py
+++ engine.py
@@ -328,7 +328,7 @@ class MultiLabelMAPEngine(Engine):
             self.state['eval_iters'] += 1
 
         z = torch.rand(ids.shape[0], 512, 768).type(torch.FloatTensor).cuda(self.state['device_ids'][0])
-        x_g = model['Generator'](z)
+        x_g = model['Generator'](z, self.state['encoded_tag'], self.state['tag_mask'])
 
         _, logits, prob = model['MABert'](ids, token_type_ids, attention_mask,
                                                                       self.state['encoded_tag'],
