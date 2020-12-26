e1eba7bce7da85f0c08f6f74685760b914e75ae7
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-26 09:52:21 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -16,7 +16,7 @@ python main.py \
         --data_type='TrainTest_agNews' \
         --data_path='../datasets/ag-news' \
         --use_previousData=1 \
-        --method='semiGAN_MultiLabelMAP' \
+        --method='MultiLabelMAP' \
         --overlength_handle='skip' \
         --min_tagFrequence=0  \
         --max_tagFrequence=99999  \
