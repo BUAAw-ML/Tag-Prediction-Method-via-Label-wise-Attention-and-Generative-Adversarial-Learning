087442319f299b628e643caac06653a9f78c7fce
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-12-31 10:33:39 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -8,7 +8,7 @@ python main.py \
         --epochs=15 \
         --epoch_step=8 \
         --device_ids=0 \
-        --batch-size=4 \
+        --batch-size=16 \
         --G-lr=0.0001 \
         --D-lr=0.01 \
         --B-lr=0.0001 \
