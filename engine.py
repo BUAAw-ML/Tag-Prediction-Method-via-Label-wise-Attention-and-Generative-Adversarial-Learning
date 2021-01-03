794eef370262c542fc918bc400b473e840cd6cea
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-03 22:16:16 +0800

    update

--- engine.py
+++ engine.py
@@ -341,6 +341,9 @@ class MultiLabelMAPEngine(Engine):
         # self.state['loss'] = torch.mean(per_example_loss)
         self.state['output'] = logits
 
+        print(target_var.shape)
+        print(self.state['output'].shape)
+
         self.state['loss'] = criterion(self.state['output'], target_var)
 
         if training:
