9c0242ca749cbe690fed382f1d0610c54e2ca6af
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-04 08:58:45 +0800

    update

--- engine.py
+++ engine.py
@@ -337,7 +337,7 @@ class MultiLabelMAPEngine(Engine):
         # self.state['output'] = F.softmax(logits, dim=-1)
         #
         log_probs = F.log_softmax(logits, dim=-1)
-        per_example_loss = -1 * torch.sum(target_var * log_probs, dim=-1) #/ target_var.shape[-1]
+        per_example_loss = -1 * torch.sum(target_var * log_probs, dim=-1) / target_var.shape[-1]
         self.state['loss'] = torch.mean(per_example_loss)
         self.state['output'] = logits
 
