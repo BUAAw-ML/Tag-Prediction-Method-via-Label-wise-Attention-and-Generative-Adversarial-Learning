96e878e4539c01c3c6b3733857aed5a23f31128c
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-04 08:11:12 +0800

    update

--- engine.py
+++ engine.py
@@ -337,7 +337,7 @@ class MultiLabelMAPEngine(Engine):
         # self.state['output'] = F.softmax(logits, dim=-1)
         #
         log_probs = F.log_softmax(logits, dim=-1)
-        per_example_loss = -1 * torch.sum(target_var * log_probs, dim=-1) / target_var.shape[-1]
+        per_example_loss = -1 * torch.sum(target_var * log_probs, dim=-1) #/ target_var.shape[-1]
         self.state['loss'] = torch.mean(per_example_loss)
         self.state['output'] = logits
 
@@ -469,13 +469,9 @@ class semiGAN_MultiLabelMAPEngine(MultiLabelMAPEngine):
 
         if semi_supervised == False: #train with labeled data
             log_probs = F.log_softmax(logits, dim=-1)
-            per_example_loss = -1 * torch.sum(target_var * log_probs, dim=-1) #/ target_var.shape[-1]
-            print(target_var.shape)
-            print(torch.sum(target_var * log_probs, dim=-1).shape)
-
+            per_example_loss = -1 * torch.sum(target_var * log_probs, dim=-1) / target_var.shape[-1]
             d_loss = torch.mean(per_example_loss)
-            print(d_loss.shape)
-            exit()
+
             # d_loss = criterion(self.state['output'], target_var) #+ D_L_unsupervised
         else:
             # pseudo_label = torch.max(self.state['output'], -1, keepdim=True)[0]
