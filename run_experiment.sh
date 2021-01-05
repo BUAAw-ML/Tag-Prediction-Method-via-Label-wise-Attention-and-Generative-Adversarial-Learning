661d0f72655413589f17dcbb92bf81ebadbb7c7b
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-05 18:01:57 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -31,7 +31,7 @@ python main.py \
         --device_ids=0 \
         --batch-size=4 \
         --G-lr=0.001 \
-        --D-lr=0.01 \
+        --D-lr=0.1 \
         --B-lr=0.01 \
         --save_model_path='./checkpoint' \
         --data_type='TrainTest_programWeb' \
