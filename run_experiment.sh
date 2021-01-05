4e7a1276b36ac12b3cfe6cc02ab47be223ce9441
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-05 18:05:59 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -37,12 +37,12 @@ python main.py \
         --data_type='TrainTest_programWeb' \
         --data_path='../datasets/ProgrammerWeb' \
         --use_previousData=0 \
-        --method='semiGAN_MultiLabelMAP' \
+        --method='MultiLabelMAP' \
         --overlength_handle='truncation' \
         --min_tagFrequence=0  \
         --max_tagFrequence=50  \
         --intanceNum_limit=99999 \
-        --data_split=1  \
+        --data_split=0.05  \
         --test_description=''  \
 
 
