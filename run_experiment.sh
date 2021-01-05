5ad4eb25050bfcd14e6932b3db2a73d2ea97a59f
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-05 17:32:10 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -3,6 +3,27 @@
 
 #EN='02'  #experiment_no
 
+#python main.py \
+#        --experiment_no='Test'  \
+#        --epochs=80 \
+#        --epoch_step=70 \
+#        --device_ids=0 \
+#        --batch-size=4 \
+#        --G-lr=0.001 \
+#        --D-lr=0.01 \
+#        --B-lr=0.001 \
+#        --save_model_path='./checkpoint' \
+#        --data_type='TrainTest_agNews' \
+#        --data_path='../datasets/ag-news' \
+#        --use_previousData=0 \
+#        --method='semiGAN_MultiLabelMAP' \
+#        --overlength_handle='skip' \
+#        --min_tagFrequence=0  \
+#        --max_tagFrequence=99999  \
+#        --intanceNum_limit=99999 \
+#        --data_split=0.0002  \
+#        --test_description=''  \
+
 python main.py \
         --experiment_no='Test'  \
         --epochs=80 \
@@ -13,8 +34,8 @@ python main.py \
         --D-lr=0.01 \
         --B-lr=0.001 \
         --save_model_path='./checkpoint' \
-        --data_type='TrainTest_agNews' \
-        --data_path='../datasets/ag-news' \
+        --data_type='TrainTest' \
+        --data_path='../datasets/ProgrammerWeb' \
         --use_previousData=0 \
         --method='semiGAN_MultiLabelMAP' \
         --overlength_handle='skip' \
@@ -23,6 +44,7 @@ python main.py \
         --intanceNum_limit=99999 \
         --data_split=0.0002  \
         --test_description=''  \
+
 
 #方法、epoch_step
 
