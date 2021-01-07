38ee94f75a25030214277c80df43df49aba9148a
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-07 11:32:33 +0800

    update

--- engine.py
+++ engine.py
@@ -486,8 +486,8 @@ class semiGAN_MultiLabelMAPEngine(MultiLabelMAPEngine):
         # print(label_mask)
 
         d_loss = D_L_unsupervised
-        if label_mask.shape != 0:
-            d_loss += criterion(logits.index_select(0, label_mask), target_var.index_select(0, label_mask)) #+ D_L_unsupervised
+        # if label_mask.shape != 0:
+        #     d_loss += criterion(logits.index_select(0, label_mask), target_var.index_select(0, label_mask)) #+ D_L_unsupervised
 
             # pseudo_label = torch.max(self.state['output'], -1, keepdim=True)[0]
             # pseudo_label = self.state['output'] - pseudo_label
