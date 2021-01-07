697cce78d50eb2912a4c3890e253b8ac2cf4aea7
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-07 09:43:40 +0800

    update

--- engine.py
+++ engine.py
@@ -480,9 +480,9 @@ class semiGAN_MultiLabelMAPEngine(MultiLabelMAPEngine):
         print(target_var.shape)
         print("----")
 
-        print(label_mask)
+        # print(label_mask[label_mask == 1])
 
-        logits.index_select(0, torch.tensor([1,3]).cuda(self.state['device_ids'][0]))
+        print(logits.index_select(0, torch.tensor([1,3]).cuda(self.state['device_ids'][0])))
         print(logits)
         print(logits.shape)
         target_var.index_select(0, torch.tensor(label_mask))
