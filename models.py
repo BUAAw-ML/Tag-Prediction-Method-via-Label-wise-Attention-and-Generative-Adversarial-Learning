149885f4914921c264a62f7e02f1b9bfcd73452a
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-08 17:23:55 +0800

    update

--- models.py
+++ models.py
@@ -100,15 +100,15 @@ class MABert(nn.Module):
 
         # prob =  torch.sigmoid(attention_out_fake)
 
-        # attention_out = torch.sum(attention_out, -1, keepdim=True)
-        # attention_out_fake = torch.sum(attention_out_fake, -1, keepdim=True)
+        attention_out = torch.sum(attention_out, -1, keepdim=True)
+        attention_out_fake = torch.sum(attention_out_fake, -1, keepdim=True)
 
         prob = torch.cat((attention_out_fake,attention_out),-1)
         # print(prob)
         # print(self.output(prob))
         prob = self.output(prob)[:,0]
 
-        prob = torch.mean(prob,-1)
+        # prob = torch.mean(prob,-1)
 
         #################
         # masks = torch.unsqueeze(attention_mask, 1)  # N, 1, L  .bool()
