c9e956f23bb143c12a4bb7b01b2b8d7f1dc7bc35
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-11 15:16:56 +0800

    update

--- models.py
+++ models.py
@@ -189,10 +189,10 @@ class MABert(nn.Module):
 
         prob = torch.sum(prob, -1, keepdim=True)
 
-        prob = torch.sigmoid(prob)
+        # prob = torch.sigmoid(prob)
 
-        # prob = torch.cat((prob,flatten),-1)
-        # prob = self.output(prob)[:,0]
+        prob = torch.cat((prob,flatten),-1)
+        prob = self.output(prob)[:,0]
         #
         #
         # prob = attention
