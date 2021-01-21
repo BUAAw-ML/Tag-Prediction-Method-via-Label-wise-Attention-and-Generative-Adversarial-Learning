af54829f2c9419b17e98341ffe96e3a51c8aaf06
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-21 11:42:59 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -37,12 +37,12 @@ python main.py \
         --B-lr=0.01 \
         --save_model_path='./checkpoint' \
         --data_type='TrainTestTextTag' \
-        --data_path='../datasets/stack-overflow' \
+        --data_path='../datasets/stack-overflow2000' \
         --use_previousData=0 \
         --method='MultiLabelMAP' \
         --overlength_handle='truncation' \
-        --min_tagFrequence=10  \
-        --max_tagFrequence=60  \
+        --min_tagFrequence=0  \
+        --max_tagFrequence=50  \
         --intanceNum_limit=99999 \
         --data_split=200  \
         --test_description=''  \
