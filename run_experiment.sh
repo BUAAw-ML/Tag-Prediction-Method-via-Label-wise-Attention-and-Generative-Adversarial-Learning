a27ebfaaeef269957be0ab1cab01edbd47573092
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-21 09:14:03 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -28,18 +28,18 @@
 
 python main.py \
         --experiment_no='zyz_test'  \
-        --epochs=100 \
-        --epoch_step=90 \
+        --epochs=50 \
+        --epoch_step=45 \
         --device_ids=0 \
         --batch-size=4 \
         --G-lr=0.001 \
         --D-lr=0.1 \
-        --B-lr=0.001 \
+        --B-lr=0.01 \
         --save_model_path='./checkpoint' \
         --data_type='TrainTestTextTag' \
         --data_path='../datasets/stack-overflow-9.5-1' \
         --use_previousData=0 \
-        --method='semiGAN_MultiLabelMAP' \
+        --method='MultiLabelMAP' \
         --overlength_handle='truncation' \
         --min_tagFrequence=0  \
         --max_tagFrequence=50  \
