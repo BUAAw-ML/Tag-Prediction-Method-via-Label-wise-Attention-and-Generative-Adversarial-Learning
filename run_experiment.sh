91c8d248c1e93e8b0ac97f0b9a5254f8dab81fc4
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-21 13:16:06 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -27,9 +27,9 @@
 
 
 python main.py \
-        --experiment_no='wcm2_tag<50'  \
+        --experiment_no='zyz_allData'  \
         --epochs=50 \
-        --epoch_step=40 \
+        --epoch_step=45 \
         --device_ids=0 \
         --batch-size=4 \
         --G-lr=0.001 \
@@ -42,52 +42,12 @@ python main.py \
         --method='MultiLabelMAP' \
         --overlength_handle='truncation' \
         --min_tagFrequence=0  \
-        --max_tagFrequence=50  \
+        --max_tagFrequence=99999  \
         --intanceNum_limit=99999 \
-        --data_split=400  \
+        --data_split=99999  \
         --test_description=''  \
 
-python main.py \
-        --experiment_no='wcm2_tag<50'  \
-        --epochs=50 \
-        --epoch_step=40 \
-        --device_ids=0 \
-        --batch-size=4 \
-        --G-lr=0.001 \
-        --D-lr=0.1 \
-        --B-lr=0.01 \
-        --save_model_path='./checkpoint' \
-        --data_type='TrainTestTextTag' \
-        --data_path='../datasets/stack-overflow2' \
-        --use_previousData=0 \
-        --method='MultiLabelMAP' \
-        --overlength_handle='truncation' \
-        --min_tagFrequence=0  \
-        --max_tagFrequence=50  \
-        --intanceNum_limit=99999 \
-        --data_split=1600  \
-        --test_description=''  \
 
-python main.py \
-        --experiment_no='wcm2_tag<50'  \
-        --epochs=50 \
-        --epoch_step=40 \
-        --device_ids=0 \
-        --batch-size=4 \
-        --G-lr=0.001 \
-        --D-lr=0.1 \
-        --B-lr=0.01 \
-        --save_model_path='./checkpoint' \
-        --data_type='TrainTestTextTag' \
-        --data_path='../datasets/stack-overflow2' \
-        --use_previousData=0 \
-        --method='MultiLabelMAP' \
-        --overlength_handle='truncation' \
-        --min_tagFrequence=0  \
-        --max_tagFrequence=50  \
-        --intanceNum_limit=99999 \
-        --data_split=6400  \
-        --test_description=''  \
 
 #方法、epoch_step
 
