bd3b36edb1844b9524ce4423bb6cf469ee4b7c46
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-25 13:35:19 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -33,7 +33,7 @@ python main.py \
         --batch-size=4 \
         --G-lr=0.001 \
         --D-lr=0.1 \
-        --B-lr=0.01 \
+        --B-lr=0.001 \
         --save_model_path='./checkpoint' \
         --data_type='TrainTest_programWeb_freecode_AAPD' \
         --data_path='../datasets/TREC-IS' \
