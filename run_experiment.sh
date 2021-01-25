38a0074de232550b8954c027fa8e014c23b99509
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-25 08:51:09 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -41,7 +41,7 @@ python main.py \
         --method='MultiLabelMAP' \
         --overlength_handle='truncation' \
         --min_tagFrequence=0  \
-        --max_tagFrequence=25  \
+        --max_tagFrequence=999999  \
         --intanceNum_limit=999999 \
         --data_split=999999  \
         --test_description=''  \
