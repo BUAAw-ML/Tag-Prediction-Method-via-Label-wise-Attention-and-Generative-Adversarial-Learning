90ca9a4619faeb1886e5ac309e0132d8d1b5dee1
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-26 09:33:49 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -27,7 +27,7 @@
 
 
 python main.py \
-        --experiment_no='wqb_MABert'  \
+        --experiment_no='hhm_MLPBert'  \
         --epochs=60 \
         --epoch_step=50 \
         --device_ids=0 \
@@ -37,7 +37,29 @@ python main.py \
         --B-lr=0.01 \
         --save_model_path='./checkpoint' \
         --data_type='TrainTest_programWeb_freecode_AAPD' \
-        --data_path='../datasets/Freecode' \
+        --data_path='../datasets/stack-overflow2000-600' \
+        --use_previousData=0 \
+        --model_type='MLPBert' \
+        --method='MultiLabelMAP' \
+        --overlength_handle='truncation' \
+        --min_tagFrequence=0  \
+        --max_tagFrequence=999999  \
+        --intanceNum_limit=999999 \
+        --data_split=999999  \
+        --test_description=''  \
+
+python main.py \
+        --experiment_no='hhm_MLPBert'  \
+        --epochs=60 \
+        --epoch_step=50 \
+        --device_ids=0 \
+        --batch-size=4 \
+        --G-lr=0.001 \
+        --D-lr=0.1 \
+        --B-lr=0.01 \
+        --save_model_path='./checkpoint' \
+        --data_type='TrainTest_programWeb_freecode_AAPD' \
+        --data_path='../datasets/stack-overflow600-' \
         --use_previousData=0 \
         --model_type='MLPBert' \
         --method='MultiLabelMAP' \
