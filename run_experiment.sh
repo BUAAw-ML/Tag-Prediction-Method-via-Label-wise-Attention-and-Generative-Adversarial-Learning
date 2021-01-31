288618021389666a467c649fa382c9df646aea61
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-31 18:16:48 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -28,13 +28,13 @@
 
 python main.py \
         --experiment_no='wqb_example'  \
-        --epochs=40 \
-        --epoch_step=37 \
+        --epochs=8 \
+        --epoch_step=7 \
         --device_ids=0 \
         --batch-size=4 \
         --G-lr=0.001 \
         --D-lr=0.1 \
-        --B-lr=0.001 \
+        --B-lr=0.01 \
         --save_model_path='./checkpoint' \
         --data_type='TrainTestTextTag' \
         --data_path='../datasets/stack-overflow2000' \
