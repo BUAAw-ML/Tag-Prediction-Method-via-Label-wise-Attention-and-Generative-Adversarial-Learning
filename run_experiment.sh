1c0241b526a24ba6cd808b3b70d058cbd5a35f10
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-31 22:16:01 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -27,7 +27,7 @@
 
 
 python main.py \
-        --experiment_no='wqb_MultiLabelMAP_example'  \
+        --experiment_no='zyc_MultiLabelMAP_example'  \
         --epochs=60 \
         --epoch_step=50 \
         --device_ids=0 \
