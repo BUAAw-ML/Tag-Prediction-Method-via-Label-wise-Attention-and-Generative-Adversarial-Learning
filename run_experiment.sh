01a16f4254579378e72701e603ad9b9778e48f1b
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-02-02 11:35:28 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,8 +1,28 @@
 # Experiment runner script
 
 #According to the relevant literature, generative adversarial learning requires a smaller learning rate
+#python main.py \
+#        --experiment_no='zyc_GAN_MultiLabelMAP'  \
+#        --epochs=100 \
+#        --epoch_step=90 \
+#        --device_ids=0 \
+#        --batch-size=4 \
+#        --G-lr=0.001 \
+#        --D-lr=0.1 \
+#        --B-lr=0.001 \
+#        --data_type='TrainTest_pkl' \
+#        --data_path='../datasets/Freecode' \
+#        --use_previousData=0 \
+#        --model_type='LABert' \
+#        --method='GAN_MultiLabelMAP' \
+#        --overlength_handle='truncation' \
+#        --min_tagFrequence=0  \
+#        --max_tagFrequence=999999  \
+#        --intanceNum_limit=999999 \
+#        --resume=''
+
 python main.py \
-        --experiment_no='zyc_GAN_MultiLabelMAP'  \
+        --experiment_no='wqb_MultiLabelMAP'  \
         --epochs=100 \
         --epoch_step=90 \
         --device_ids=0 \
@@ -14,32 +34,12 @@ python main.py \
         --data_path='../datasets/Freecode' \
         --use_previousData=0 \
         --model_type='LABert' \
-        --method='GAN_MultiLabelMAP' \
+        --method='MultiLabelMAP' \
         --overlength_handle='truncation' \
         --min_tagFrequence=0  \
         --max_tagFrequence=999999  \
         --intanceNum_limit=999999 \
         --resume=''
-
-#python main.py \
-#        --experiment_no='hhm_MultiLabelMAP'  \
-#        --epochs=50 \
-#        --epoch_step=40 \
-#        --device_ids=0 \
-#        --batch-size=4 \
-#        --G-lr=0.001 \
-#        --D-lr=0.1 \
-#        --B-lr=0.01 \
-#        --data_type='TrainTest_pkl' \
-#        --data_path='../datasets/Freecode' \
-#        --use_previousData=0 \
-#        --model_type='LABert' \
-#        --method='MultiLabelMAP' \
-#        --overlength_handle='truncation' \
-#        --min_tagFrequence=0  \
-#        --max_tagFrequence=999999  \
-#        --intanceNum_limit=999999 \
-#        --resume=''
 
 #model_type: MLPBert, LABert
 
