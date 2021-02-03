22e8ab66c7d0531a4bbd592ec3b4a5bdfdb2b4e8
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-02-03 08:15:38 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,29 +1,9 @@
 # Experiment runner script
 
-python main.py \
-        --experiment_no='wcm_GAN_MultiLabelMAP'  \
-        --epochs=100 \
-        --epoch_step=90 \
-        --device_ids=0 \
-        --batch-size=8 \
-        --G-lr=0.001 \
-        --D-lr=0.1 \
-        --B-lr=0.01 \
-        --data_type='TrainTest_pkl' \
-        --data_path='../datasets/Freecode' \
-        --use_previousData=0 \
-        --model_type='LABert' \
-        --method='GAN_MultiLabelMAP' \
-        --overlength_handle='truncation' \
-        --min_tagFrequence=0  \
-        --max_tagFrequence=999999  \
-        --intanceNum_limit=999999 \
-        --resume=''
-
 #python main.py \
-#        --experiment_no='LABert'  \
-#        --epochs=50 \
-#        --epoch_step=40 \
+#        --experiment_no='wcm_GAN_MultiLabelMAP'  \
+#        --epochs=100 \
+#        --epoch_step=90 \
 #        --device_ids=0 \
 #        --batch-size=8 \
 #        --G-lr=0.001 \
@@ -33,12 +13,32 @@ python main.py \
 #        --data_path='../datasets/Freecode' \
 #        --use_previousData=0 \
 #        --model_type='LABert' \
-#        --method='MultiLabelMAP' \
+#        --method='GAN_MultiLabelMAP' \
 #        --overlength_handle='truncation' \
 #        --min_tagFrequence=0  \
 #        --max_tagFrequence=999999  \
 #        --intanceNum_limit=999999 \
 #        --resume=''
+
+python main.py \
+        --experiment_no='wqb_LABert'  \
+        --epochs=50 \
+        --epoch_step=40 \
+        --device_ids=0 \
+        --batch-size=8 \
+        --G-lr=0.001 \
+        --D-lr=0.1 \
+        --B-lr=0.01 \
+        --data_type='TrainTest_pkl' \
+        --data_path='../datasets/Freecode' \
+        --use_previousData=0 \
+        --model_type='LABert' \
+        --method='MultiLabelMAP' \
+        --overlength_handle='truncation' \
+        --min_tagFrequence=0  \
+        --max_tagFrequence=999999  \
+        --intanceNum_limit=999999 \
+        --resume=''
 
 #model_type: MLPBert, LABert
 #method: MultiLabelMAP GAN_MultiLabelMAP
